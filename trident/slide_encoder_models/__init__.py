8358fe1e80fe2175583160b6946f77f5634f6a64
aspartate <61163004+aspartate@users.noreply.github.com>
2025-03-06 09:33:44 -0500

    add MadeleineSlideEncoder to __init__.py

--- trident/slide_encoder_models/__init__.py
+++ trident/slide_encoder_models/__init__.py
@@ -8,6 +8,7 @@ from trident.slide_encoder_models.load import (
     GigaPathSlideEncoder,
     TitanSlideEncoder,
     ThreadsSlideEncoder,
+    MadeleineSlideEncoder,
 )
 
 __all__ = [
@@ -19,4 +20,5 @@ __all__ = [
     "GigaPathSlideEncoder",
     "TitanSlideEncoder",
     "ThreadsSlideEncoder",
+    "MadeleineSlideEncoder",
 ]
